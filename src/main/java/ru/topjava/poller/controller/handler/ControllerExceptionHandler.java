a15db24bc36ff0c189a19c22b9dd2864b62cf05d
Pavel Teryoshin <116794811+pteryo@users.noreply.github.com>
2024-09-27 19:57:10 +0300

    add vote db annotations fill ControllerExceptionHandler

--- src/main/java/ru/topjava/poller/controller/handler/ControllerExceptionHandler.java
+++ src/main/java/ru/topjava/poller/controller/handler/ControllerExceptionHandler.java
@@ -2,15 +2,36 @@ package ru.topjava.poller.controller.handler;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import ru.topjava.poller.controller.exception.NotFoundException;
 
+@ControllerAdvice
 public class ControllerExceptionHandler {
 
     @ExceptionHandler(NotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ResponseEntity<String> handleNotFound(NotFoundException ex) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Object> handleGlobalException(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(HttpMediaTypeNotSupportedException.class)
+    @ResponseStatus(HttpStatus.UNSUPPORTED_MEDIA_TYPE)
+    public ResponseEntity<String> handleValidationExceptions(HttpMediaTypeNotSupportedException ex) {
+        return ResponseEntity.status(HttpStatus.UNSUPPORTED_MEDIA_TYPE).body(ex.getMessage());
     }
 }
--- src/main/java/ru/topjava/poller/model/Vote.java
+++ src/main/java/ru/topjava/poller/model/Vote.java
@@ -1,11 +1,21 @@
 package ru.topjava.poller.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 import ru.topjava.poller.model.Place;
 import ru.topjava.poller.model.User;
 
+@Entity
+@Table(name = "votes")
 public class Vote {
-
+    @OneToOne
+    @JoinColumn(name = "user_id", nullable = false)
     private User user;
+
+    @OneToOne
+    @JoinColumn(name = "place_id", nullable = false)
     private Place Place;
 
     public User getUser() {
